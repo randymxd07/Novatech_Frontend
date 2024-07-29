1abab8ac8a79f404148bfb856bc407535d8d624d
randymxd06 <randym0624@gmail.com>
2024-07-29 18:48:36 -0400

    feat: :sparkles: Separate chat styles from the main screen

--- src/presentation/layouts/ChatLayout.tsx
+++ src/presentation/layouts/ChatLayout.tsx
@@ -1,21 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import './ChatLayout.css';
 
 export const ChatLayout = () => {
+
+  useEffect(() => {
+
+    document.body.classList.add('chat-layout-styles');
+
+    return () => {
+      document.body.classList.remove('chat-layout-styles');
+    };
+
+  }, []);
 
   return (
 
     <main className="flex flex-row mt-7">
 
-      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)]  bg-white bg-opacity-10 p-5 rounded-3xl">
+      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)] bg-white bg-opacity-10 p-5 rounded-3xl">
+
         <div className="flex flex-row h-full">
+
           <div className="flex flex-col flex-auto h-full p-1">
+
             <Outlet />
+
           </div>
+
         </div>
+
       </section>
 
     </main>
-
+    
   );
 
 };
