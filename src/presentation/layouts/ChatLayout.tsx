548dccc073d135cc0ab10a0b0025b12e7d6fc2c4
randymxd06 <randym0624@gmail.com>
2024-07-31 14:08:45 -0400

    style: :lipstick: I think I finished fixing the chat darkmode, only the input is missing

--- src/presentation/layouts/ChatLayout.tsx
+++ src/presentation/layouts/ChatLayout.tsx
@@ -13,38 +13,25 @@ export const ChatLayout = () => {
     localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
     if (isDarkMode) {
       document.body.classList.add('dark');
+      document.body.classList.add('bg-gray-900');
+      document.body.classList.add('text-white');
     } else {
+      document.body.classList.add('bg-white');
       document.body.classList.remove('dark');
+      document.body.classList.remove('bg-gray-900');
+      document.body.classList.remove('text-white');
     }
   }, [isDarkMode]);
 
-  useEffect(() => {
-    document.body.classList.add('chat-layout-styles');
-    return () => {
-      document.body.classList.remove('chat-layout-styles');
-    };
-  }, []);
-
   return (
-
     <main className={`font-sans flex flex-row mt-7 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
-
       <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)] bg-opacity-10 p-5 rounded-3xl">
-
         <div className="flex flex-row h-full">
-
           <div className="flex flex-col flex-auto h-full p-1">
-
             <Outlet />
-
           </div>
-
         </div>
-
       </section>
-
     </main>
-
   );
-
 };
