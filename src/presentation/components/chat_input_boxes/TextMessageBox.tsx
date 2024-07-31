abcc2cea18e7c5e6df3f2daf62adac45cc570b56
randymxd06 <randym0624@gmail.com>
2024-07-31 14:17:36 -0400

    style: :lipstick: I'm going to work on the chat design in lightmode

--- src/presentation/components/chat_input_boxes/TextMessageBox.tsx
+++ src/presentation/components/chat_input_boxes/TextMessageBox.tsx
@@ -23,33 +23,28 @@ export const TextMessageBox = ({ onSendMessage, placeholder, disableCorrections 
     }
 
     return (
-        <form
-            onSubmit={ handleSendMessage }
-            className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4"
-        >
 
+        <form
+            onSubmit={handleSendMessage}
+            className="flex flex-row items-center h-16 rounded-xl bg-white dark:bg-gray-700 w-full px-4"
+        >
             {/* MESSAGE INPUT */}
             <div className="flex-grow">
-
                 <div className="relative w-full">
-
-                    <input 
+                    <input
                         type="text"
                         autoFocus
                         name="message"
-                        className="flex w-full border rounder-xl text-gray-800 focus:outline-none focus:border-indigo-300 pl-4 h-10"
-                        placeholder={ placeholder }
-                        autoComplete={ disableCorrections ? 'on' : 'off' }
-                        autoCorrect={ disableCorrections ? 'on' : 'off' }
-                        spellCheck={ disableCorrections ? 'true' : 'false' }
-                        value={ message }
-                        onChange={ (e) => setMessage(e.target.value) }
+                        className="flex w-full border rounded-xl border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 text-gray-800 focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                        placeholder={placeholder}
+                        autoComplete={disableCorrections ? 'on' : 'off'}
+                        autoCorrect={disableCorrections ? 'on' : 'off'}
+                        spellCheck={disableCorrections ? 'true' : 'false'}
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
                     />
-                    
                 </div>
-
             </div>
-
             {/* SEND BUTTON */}
             <div className="ml-4">
                 <button className="btn-primary">
@@ -57,7 +52,8 @@ export const TextMessageBox = ({ onSendMessage, placeholder, disableCorrections 
                     <i className="fa-regular fa-paper-plane"></i>
                 </button>
             </div>
-
         </form>
+
     )
+
 }
