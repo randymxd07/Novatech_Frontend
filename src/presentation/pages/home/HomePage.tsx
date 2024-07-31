187d357700db051952ebf0996685748bd978cd6f
randymxd06 <randym0624@gmail.com>
2024-07-30 20:50:27 -0400

    style: :lipstick: Add message section

--- src/presentation/pages/home/HomePage.tsx
+++ src/presentation/pages/home/HomePage.tsx
@@ -1020,7 +1020,7 @@ const HomePage = () => {
                                     </ul>
                                 </div>
                             </div>
-                            
+
                         </div>
 
                     </div>
@@ -1029,15 +1029,47 @@ const HomePage = () => {
 
             </section>
             
-            {/* <section className="bg-gray-50 dark:bg-gray-800">
+            {/*===========
+                MESSAGE
+            ==============*/}
+            <section className="bg-gray-50 dark:bg-gray-800">
+
+                {/*===========
+                    CONTENT
+                ==============*/}
                 <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
+
+                    {/*=============
+                        CONTAINER
+                    ================*/}
                     <div className="max-w-screen-sm mx-auto text-center">
-                        <h2 className="mb-4 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">Start your free trial today</h2>
-                        <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Try Landwind Platform for 30 days. No credit card required.</p>
-                        <a href="#" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Free trial for 30 days</a>
+
+                        {/*=========
+                            TITLE
+                        ============*/}
+                        <h2 className="mb-4 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
+                            Start your free trial today
+                        </h2>
+
+                        {/*========
+                            TEXT
+                        ===========*/}
+                        <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">
+                            Try Landwind Platform for 30 days. No credit card required.
+                        </p>
+
+                        {/*=================================
+                            FREE TRIAL FOR 30 DAYS BUTTON
+                        ====================================*/}
+                        <a href="#" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                            Free trial for 30 days
+                        </a>
+
                     </div>
+
                 </div>
-            </section> */}
+
+            </section>
 
             {/* <footer className="bg-white dark:bg-gray-800">
                 <div className="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
