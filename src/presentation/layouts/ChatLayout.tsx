d05c61f3200ff1a8bde8eb3eb19d36164e3ec6b9
randymxd06 <randym0624@gmail.com>
2024-07-31 13:51:23 -0400

    style: :lipstick: I'm fixing the light and darkmode of the chat

--- src/presentation/layouts/ChatLayout.tsx
+++ src/presentation/layouts/ChatLayout.tsx
@@ -1,24 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import './ChatLayout.css';
 
 export const ChatLayout = () => {
 
+  const [isDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    return savedMode ? JSON.parse(savedMode) : window.matchMedia('(prefers-color-scheme: dark)').matches;
+  });
+
   useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
+    if (isDarkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [isDarkMode]);
 
+  useEffect(() => {
     document.body.classList.add('chat-layout-styles');
-
     return () => {
       document.body.classList.remove('chat-layout-styles');
     };
-
   }, []);
 
   return (
 
-    <main className="flex flex-row mt-7">
+    <main className={`font-sans flex flex-row mt-7 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
 
-      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)] bg-white bg-opacity-10 p-5 rounded-3xl">
+      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)] bg-opacity-10 p-5 rounded-3xl">
 
         <div className="flex flex-row h-full">
 
@@ -33,7 +44,7 @@ export const ChatLayout = () => {
       </section>
 
     </main>
-    
+
   );
 
 };
