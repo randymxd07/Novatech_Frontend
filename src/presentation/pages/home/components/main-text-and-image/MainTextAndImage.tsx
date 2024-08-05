633b16fff5ce0aa6f465e05b626c680d46da3df6
randymxd06 <randym0624@gmail.com>
2024-08-05 19:44:59 -0400

    feat: :sparkles: Finish with the brands part

--- src/presentation/pages/home/components/main-text-and-image/MainTextAndImage.tsx
+++ src/presentation/pages/home/components/main-text-and-image/MainTextAndImage.tsx
@@ -18,14 +18,14 @@ export const MainTextAndImage = () => {
                     {/*=========
                         TITLE
                     ============*/}
-                    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+                    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white select-none">
                         Transforma tu negocio con nosotros.
                     </h1>
 
                     {/*=============
                         MAIN TEXT
                     ================*/}
-                    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 select-none">
                         Esta es la platforma perfecta para que comiences a realizar la transformación digital de tu empresa, NOVATECH es una plataforma que te brinda soporte técnico las 24 horas del día, si no tienes departamento de tecnología o necesitas algún consejo tecnológico esta es tu plataforma.
                     </p>
 
@@ -37,14 +37,14 @@ export const MainTextAndImage = () => {
                         {/*=========================
                             VIEW ON GITHUB BUTTON
                         ============================*/}
-                        <a className="cursor-pointer inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center bg-blue-600 text-white border border-gray-200 rounded-lg sm:w-auto hover:bg-blue-700 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-blue-700 dark:focus:ring-gray-800">
+                        <a className="cursor-pointer inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center bg-blue-600 text-white border border-gray-200 rounded-lg sm:w-auto hover:bg-blue-700 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-blue-700 dark:focus:ring-gray-800 select-none">
                             Comience la prueba gratuita de 30 días
                         </a>
 
                         {/*=========================
                             GET FIGMA FILE BUTTON
                         ============================*/}
-                        <a className="cursor-pointer inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                        <a className="cursor-pointer inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 select-none">
                             Precios & Preguntas Frecuentes
                         </a>
 
