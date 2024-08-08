b1eabf7c2119922ba89ea2ea8868d31c4cefa971
randymxd06 <randym0624@gmail.com>
2024-08-08 14:10:03 -0400

    feat: :sparkles: Fix mobile view menu

--- src/presentation/pages/home/components/headers/Header.tsx
+++ src/presentation/pages/home/components/headers/Header.tsx
@@ -126,7 +126,7 @@ export const Header = () => {
                             onClick={toggleMenu}
                             data-collapse-toggle="mobile-menu-2"
                             type="button"
-                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 self-end"
                             aria-controls="mobile-menu-2"
                             aria-expanded={isMenuOpen ? 'true' : 'false'}
                         >
