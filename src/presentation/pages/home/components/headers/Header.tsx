a35580d73ad734930dd26b24fc75f3a2d5ac5c5c
randymxd06 <randym0624@gmail.com>
2024-08-08 00:39:07 -0400

    style: :lipstick: Add an icon to the login button

--- src/presentation/pages/home/components/headers/Header.tsx
+++ src/presentation/pages/home/components/headers/Header.tsx
@@ -104,8 +104,13 @@ export const Header = () => {
                         {/*================
                             LOGIN BUTTON 
                         ===================*/}
-                        <a href="#" className="text-gray-800 bg-gray-300 hover:bg-gray-200 focus:ring-gray-300 dark:text-whitefocus:ring-4 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2">
-                            Iniciar Sesion
+                        <a href="#" className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-blue-300 dark:text-white focus:ring-4 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 flex items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-white h-5">
+                                <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z" />
+                            </svg>
+                            <span className="text-md">
+                                Iniciar Sesion
+                            </span>
                         </a>
 
                         {/*====================
